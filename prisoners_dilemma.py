d9a1ed12fe2563fdeafc7dea5870fd0223b29eef
71001014 <seth.snider@students.sacs.k12.in.us>
2015-12-16 09:48:12 -0500

    comment change
    comment change

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 
 ''' 
+
+Contributers: Seth Snider, 
 PrisonerDilemma.py allows hard-coding different strategiess
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
 Each strategy plays 100 to 200 rounds against each other strategy.
